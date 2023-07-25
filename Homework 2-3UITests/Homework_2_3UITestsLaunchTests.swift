ba3e60ebd29bb0d9c83d1ffbc30ac0779d6ecbfd
dima <dima@kojem.ru>
2023-07-25 17:16:20 +0300

    Initial Commit

---
+++ Homework 2-3UITests/Homework_2_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Homework_2_3UITestsLaunchTests.swift
+//  Homework 2-3UITests
+//
+//  Created by Дима Кожемякин on 25.07.2023.
+//
+
+import XCTest
+
+final class Homework_2_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
