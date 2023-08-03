9721522249713b9658acdf792d7f550404bf0c12
dima <dima@kojem.ru>
2023-08-03 13:04:47 +0300

    jd

--- Homework 2-3/ViewController.swift
+++ Homework 2-3/ViewController.swift
@@ -1,19 +1,60 @@
-//
-//  ViewController.swift
-//  Homework 2-3
-//
-//  Created by Дима Кожемякин on 25.07.2023.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sliderG: UISlider!
+    
+    @IBOutlet weak var sliderB: UISlider!
+    
+    @IBOutlet weak var sliderR: UISlider!
+    
+    @IBOutlet weak var lableBN: UILabel!
+    
+    @IBOutlet weak var lableGN: UILabel!
+    
+    @IBOutlet weak var lableRN: UILabel!
+    
+    @IBOutlet weak var viewColor: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewStartColor()
+        
+        
+        
+        
+        
+        viewColor.layer.cornerRadius = 30
+        
+        sliderR.minimumValue = 0
+        sliderR.maximumValue = 255
+        
+        sliderG.minimumValue = 0
+        sliderG.maximumValue = 255
+        
+        sliderB.minimumValue = 0
+        sliderB.maximumValue = 255
+        
     }
-
-
+    func viewStartColor() {
+        viewColor.backgroundColor = .red
+        sleep(1)
+        viewColor.backgroundColor = .blue
+        sleep(1)
+        viewColor.backgroundColor = .yellow
+        sleep(1)
+        viewColor.backgroundColor = .purple
+        
+        
+    }
+    
+    
+    
+    
+    
 }
 
